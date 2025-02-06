0613b4a04844d889808194dbb6e3a1a9803c2977
Keolin Moonsamy <impulse372@gmail.com>
2025-02-06 16:09:33 +0200

    Added files to repository

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CosmosChartDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.startDate = new System.Windows.Forms.DateTimePicker();
+            this.endDate = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(81, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(810, 283);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // startDate
+            // 
+            this.startDate.Location = new System.Drawing.Point(512, 324);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(200, 20);
+            this.startDate.TabIndex = 3;
+            // 
+            // endDate
+            // 
+            this.endDate.Location = new System.Drawing.Point(512, 361);
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(200, 20);
+            this.endDate.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 458);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(989, 502);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.endDate);
+            this.Controls.Add(this.startDate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "CosmosDBTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker startDate;
+        private System.Windows.Forms.DateTimePicker endDate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+namespace CosmosChartDisplay
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient _httpClient;
+        public Form1()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> array1 = new List<int>();
+            array1.Add(1);
+            array1.Add(2);
+            array1.Add(3);
+            array1.Add(4);
+
+            chart1.Series.Clear();
+            var series = new Series("Data")
+            {
+                ChartType = SeriesChartType.Line
+            };
+            
+            int i = 0;
+            foreach (var item in array1)
+            {
+                i++;
+                series.Points.AddXY(i, item);
+            }
+
+            chart1.Series.Add(series);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string baseUrl = "URL";
+            string bearerToken = "bearerToken";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+            string deviceId = textBox1.Text;
+            string StartDate = startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss");
+            string EndDate = endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss");
+            string dataKey = textBox2.Text;
+            
+            string apiUrl = $"{baseUrl}?deviceId={deviceId}&startDate={StartDate}&endDate={EndDate}&Data={dataKey}";
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string response = await _httpClient.GetStringAsync(apiUrl);
+            stopwatch.Stop();
+
+            label1.Text = stopwatch.Elapsed.TotalMilliseconds.ToString() + " ms for query to be done";
+            var jsonData = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(response);
+
+            chart1.Series.Clear();
+            var series = new Series("Data")
+            {
+                ChartType = SeriesChartType.Line
+            };
+
+            // Calculate the difference between the first two timestamps
+            long tsDifference = 0;
+            if (jsonData.Count >= 2) // Ensure there are at least two records
+            {
+                var firstItem = jsonData[10];
+                var secondItem = jsonData[11];
+
+                if (firstItem.TryGetValue("ts", out var firstTsElement) &&
+                    secondItem.TryGetValue("ts", out var secondTsElement) &&
+                    firstTsElement is JsonElement firstTsJson &&
+                    secondTsElement is JsonElement secondTsJson &&
+                    firstTsJson.TryGetInt64(out long firstTs) &&
+                    secondTsJson.TryGetInt64(out long secondTs))
+                {
+                    // Calculate the timestamp difference
+                    tsDifference = secondTs - firstTs;
+                }
+            }
+
+            if (tsDifference >= 60) { 
+                tsDifference = tsDifference / 60;
+                label2.Text = tsDifference.ToString();
+                label2.Text = label2.Text + " minute transmission intervals";
+            }
+            else
+            {
+                label2.Text = tsDifference.ToString();
+                label2.Text = label2.Text + " second transmission intervals";
+            }
+
+            int itemCount = 0;
+
+            foreach (var item in jsonData)
+            {
+                // Check if the JSON object contains "ts" and "pSC" fields
+                if (item.TryGetValue("ts", out var tsElement) &&
+                    item.TryGetValue(dataKey, out var dataElement) &&
+                    tsElement is JsonElement tsJson &&
+                    dataElement is JsonElement dataJson)
+                {
+                    // Extract values and add to the chart
+                    if (tsJson.TryGetInt64(out long ts) &&
+                        dataJson.TryGetInt32(out int data))
+                    {
+                        itemCount++;
+                        var dateTime = DateTimeOffset.FromUnixTimeSeconds(ts).UtcDateTime.AddHours(2);
+                        series.Points.AddXY(dateTime, data);
+                    }
+                }
+            }
+            chart1.Series.Add(series);
+
+            label3.Text = $"{itemCount} number of records for device";
+        }
+    }
+
+    public class ApiResponse
+    {
+        public Dictionary<string, object> Data { get; set; }
+        public DateTime PublishedAt { get; set; }
+    }
+}
